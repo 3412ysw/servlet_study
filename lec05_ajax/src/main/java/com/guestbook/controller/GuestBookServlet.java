74c4198534ffe0a306f3fe6b24d10cc01d2b2aeb
3412ysw <3412ysw@gmail.com>
2025-02-12 14:09:54 +0900

    [ysw]2/12

---
+++ lec05_ajax/src/main/java/com/guestbook/controller/GuestBookServlet.java
@@ -0,0 +1,42 @@
+package com.guestbook.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+
+
+@WebServlet("/guestBook")
+public class GuestBookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public GuestBookServlet() {
+        super();
+    }
+
+	
+	@SuppressWarnings("unchecked")
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String guestName = request.getParameter("guestName");
+		String guestMessage = request.getParameter("guestMessage");
+		
+		JSONObject g1 = new JSONObject();
+		g1.put("name", guestName);
+		g1.put("msg", guestMessage);
+		
+		response.setContentType("application/json; charset=utf-8");
+		response.getWriter().print(g1); 
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
