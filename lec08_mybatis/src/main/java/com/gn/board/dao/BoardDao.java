59265c7b654525d95dab79633aabc3a5ed2d0781
3412ysw <3412ysw@gmail.com>
2025-02-21 15:53:57 +0900

    [ysw]2/21

--- lec08_mybatis/src/main/java/com/gn/board/dao/BoardDao.java
+++ lec08_mybatis/src/main/java/com/gn/board/dao/BoardDao.java
@@ -1,6 +1,7 @@
 package com.gn.board.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
@@ -8,9 +9,40 @@ import com.gn.board.vo.Board;
 
 public class BoardDao {
 
-	public List<Board> selectBoardList(SqlSession session){
+	public List<Board> selectBoardList(SqlSession session,Board option){
 		// 매개변수 : mapper의 namespace.쿼리문의 id 
-		return session.selectList("boardMapper.boardList");
+		return session.selectList("boardMapper.boardList",option);
 	}
 	
+	public Board selectBoardOne(SqlSession session, int boardNo) {
+		return session.selectOne("boardMapper.boardOne",boardNo);
+	}
+	
+	public Board selectBoardTwo(SqlSession session, Map<String,String> paramMap) {
+		return session.selectOne("boardMapper.boardTwo",paramMap);
+	}
+	
+	public Board selectBoardThree(SqlSession session, Board option) {
+		return session.selectOne("boardMapper.boardThree",option);
+	}
+	
+	public int updateBoard(SqlSession session, Board board) {
+		return session.update("boardMapper.boardUpdate", board);
+	}
+	
+	public int deleteBoard(SqlSession session, int boardNo) {
+		return session.update("boardMapper.boardDelete",boardNo);
+	}
+
+	public int createBoard(SqlSession session, Board board) {
+		System.out.println("실행 전 : " + board);
+		int result =  session.insert("boardMapper.boardCreate", board);
+		System.out.println("실행 후  : " + board);
+		result = board.getBoardNo();
+		return result;
+	}
+	
+	public int insertMany(SqlSession session, List<Board> list) {
+		return session.insert("boardMapper.insertMany",list);
+	}
 }
--- lec08_mybatis/src/main/java/com/gn/board/service/BoardService.java
+++ lec08_mybatis/src/main/java/com/gn/board/service/BoardService.java
@@ -3,6 +3,7 @@ package com.gn.board.service;
 import static com.gn.common.sql.SqlSessionTemplate.getSqlSeeeion;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
@@ -11,10 +12,61 @@ import com.gn.board.vo.Board;
 
 public class BoardService {
 	
-	public List<Board> selectBoardList(){
+	public List<Board> selectBoardList(Board option){
 		SqlSession session = getSqlSeeeion();
-		List<Board> resultList = new BoardDao().selectBoardList(session);
+		List<Board> resultList = new BoardDao().selectBoardList(session,option);
 		session.close();
 		return resultList;
+	}
+	
+	public Board selectBoardOne(int boardNo) {
+		SqlSession session = getSqlSeeeion();
+		Board board = new BoardDao().selectBoardOne(session,boardNo);
+		session.close();
+		return board;
+	}
+	
+	public Board selectBoardTwo(Map<String,String> paramMap) {
+		SqlSession session = getSqlSeeeion();
+		Board board = new BoardDao().selectBoardTwo(session,paramMap);
+		session.close();
+		return board;
+	}
+	
+	public Board selectBoardThree(Board option) {
+		SqlSession session = getSqlSeeeion();
+		Board board = new BoardDao().selectBoardThree(session,option);
+		session.close();
+		return board;
+	}
+	
+	public int updateBoard(Board board) {
+		SqlSession session = getSqlSeeeion();
+		int result = new BoardDao().updateBoard(session,board);
+//		session.commit(true);
+//		session.rollback();
+		session.close();
+		return result;
+	}
+	
+	public int deleteBoard(int boardNo) {
+		SqlSession session = getSqlSeeeion();
+		int result = new BoardDao().deleteBoard(session,boardNo);
+		session.close();
+		return result;
+	}
+	
+	public int createBoard(Board board) {
+		SqlSession session = getSqlSeeeion();
+		int result = new BoardDao().createBoard(session,board);
+		session.close();
+		return result;
+	}
+	
+	public int insertMany(List<Board> list) {
+		SqlSession session = getSqlSeeeion();
+		int result = new BoardDao().insertMany(session,list);
+		session.close();
+		return result;
 	}
 }
